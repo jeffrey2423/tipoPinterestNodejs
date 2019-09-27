78e23ac113dfc7be3907f3d27782b2099ec05c1f
JeffreyRios24 <jeffrey.rios@correounivalle.edu.co>
2019-09-27 16:30:45 -0500

    conexion bd agregada

--- src/database.js
+++ src/database.js
@@ -1,1 +1,7 @@
 const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/copia_pinterest', {
+    useNewUrlParser: true
+})
+    .then(db => console.log('db is connected'))
+    .catch(err => console.log(err));
